b99801a96d11800296f7b26a7d1468ae6c9e46ca
samanthaCm <samanthacmsi@gmail.com>
2021-12-08 11:35:48 -0600

    Set router v6 and components

---
+++ src/Router.js
@@ -0,0 +1,35 @@
+//imports
+import { Outlet } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+//Dummies components
+
+const Home = () => <h1>Home</h1>;
+const Layout = () => {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <Outlet />
+    </div>
+  );
+};
+const About = () => <h1>About</h1>;
+const Pokemons = () => <h1>Pokemons</h1>;
+
+const Router = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/pokemons" element={<Pokemons />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default Router;
---
+++ src/pages/Pokemons.js
@@ -0,0 +1,5 @@
+const Pokemons = () => {
+  return <div>Pokemon</div>;
+};
+
+export default Pokemons;
