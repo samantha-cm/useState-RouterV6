c9f6e5006fe6ea359c03907f0dee9cb4439428e9
samanthaCm <samanthacmsi@gmail.com>
2021-12-08 11:55:13 -0600

    add nav with links to

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,23 @@
 //imports
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Pokemons from "./pages/Pokemons";
+import Layout from "./pages/Layout";
 //Dummies components
 
-const Home = () => <h1>Home</h1>;
-const Layout = () => {
-  return (
-    <div>
-      <h1>Layout</h1>
-      <Outlet />
-    </div>
-  );
-};
-const About = () => <h1>About</h1>;
-const Pokemons = () => <h1>Pokemons</h1>;
+// const Home = () => <h1>Home</h1>;
+// const Layout = () => {
+//   return (
+//     <div>
+//       <h1>Layout</h1>
+//       <Outlet />
+//     </div>
+//   );
+// };
+// const About = () => <h1>About</h1>;
+// const Pokemons = () => <h1>Pokemons</h1>;
 
 const Router = () => {
   return (
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,5 +1,21 @@
+import { Outlet, Link } from "react-router-dom";
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <>
+      <h1>Layout</h1>
+      <nav>
+        <ul>
+          <Link to="/">Home</Link>
+          <br />
+          <Link to="/about"> About</Link>
+          <br />
+          <Link to="/pokemons"> Pokemons</Link>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
 };
 
 export default Layout;
